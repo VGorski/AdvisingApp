37e0304e2238da917f5b407aa75eee22c249ba5c
VGorski <54547812+VGorski@users.noreply.github.com>
2021-11-16 20:33:16 -0500

    Added in hashing and salting user password

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -4,6 +4,7 @@ var loginRoute = express.Router();
 // Add Microsoft database server here
 var user = require('../models/advisorModel');
 var jsonToken = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 loginRoute
   .route("/")
@@ -47,28 +48,36 @@ loginRoute
     res.end();
   })
   .post((req, res, next) => {
-    // Create a new user with the gathered information
-    var newUser = new user({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      role: req.body.role
-    });
+    // Number of salt rounds
+    const salts = 10;
+    const password = req.body.password;
 
-    // Check to see if this user exists or not
-    let promise = new Promise(newUser);
-    
-    // User does not exist so a new account can be created
-    promise.then(function (checkUser) {
-      return res.status(200).json(checkUser);
-    })
+    // Hash and salt the password before storing it with the user info
+    bcrypt.hash(password, salts, function (err, hashSalt) {
+      // Create a new user with the gathered information
+      var newUser = new user({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashSalt,
+        role: req.body.role
+      });
+      
+        if (err) throw err;
+  
+      // Check to see if this user exists or not
+      let promise = new Promise(newUser);
+      
+      // User does not exist so a new account can be created
+      promise.then(function (checkUser) {
+        return res.status(200).json(checkUser);
+      })
 
-    // User already exists so return an error
-    promise.catch(function (err) {
-      return res.status(500).json({message: 'This user already exists!'})
-    })
-
+      // User already exists so return an error
+      promise.catch(function (err) {
+        return res.status(500).json({message: 'This user already exists!'})
+      })
+  });
     res.end();
   });
 

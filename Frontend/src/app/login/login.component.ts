62ec7f42e087de802017125b59cc8a07543c1396
VGorski <54547812+VGorski@users.noreply.github.com>
2021-12-01 09:10:59 -0500

    Created login page

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  QUEmail: string = '';
+  password: string = '';
+  isRealEmail: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  checkIfString(event: any, type: string) {
+    if (type === 'QUEmail') {
+      this.QUEmail = event.target.value;
+    } else if (type === 'password') {
+      this.password = event.target.value;
+    }
+  }
+
+  validateEmail(): void {
+    var checkSymbols = RegExp(/@/);
+    if (checkSymbols.test(this.QUEmail)) {
+      this.isRealEmail = true;
+    } else {
+      this.isRealEmail = false;
+    }
+  }
 }

53c8a39decf0e68cd82fd479344e6ea00795b195
VGorski <54547812+VGorski@users.noreply.github.com>
2021-12-07 20:41:47 -0500

    Created advisee dashboard and schedule viewer

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router, RouterModule, RouterOutlet, Routes } from '@angular/router';
 //import { AppRoutingModule } from './app-routing.module';
+import { appRouting } from './app.routing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdviseeDashboardComponent } from './advisee-dashboard/advisee-dashboard.component';
 import { AdviseeScheduleComponent } from './advisee-schedule/advisee-schedule.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'adviseeDashboard', component: AdviseeDashboardComponent },
-  { path: 'adviseeSchedule', component: AdviseeScheduleComponent}
-];
+
 
 
 @NgModule({
@@ -24,8 +20,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    //AppRoutingModule,
-    RouterModule.forRoot(routes)
+    appRouting,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
